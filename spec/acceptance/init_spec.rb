0eadb2a4cb92ebe5da56a8f5fb43ee377b4ca3eb
Daniel Dreier <ddreier@thinkplango.com>
2014-06-22 12:14:30 -0700

    Improve beaker test coverage Add test cases installing ruby 1.9.1 and 1.8.7, similar to examples from README.md

--- spec/acceptance/init_spec.rb
+++ spec/acceptance/init_spec.rb
@@ -17,10 +17,54 @@ describe 'ruby class' do
         should be_installed
       }
     end
-
   end
 
-  context 'with rubygems' do
+  context 'with alternate packages specified' do
+    it 'should run with no errors' do
+      pp = <<-EOS
+      class { 'ruby':
+        ruby_package     => 'ruby1.9.1-full',
+        rubygems_package => 'rubygems1.9.1',
+        gems_version     => 'latest',
+      }
+      EOS
+
+      # Run it twice and test for idempotency
+      apply_manifest(pp, :catch_failures => true)
+
+      skip "fix recurring \"ensure changed 'purged' to 'latest' \" on each run"
+      expect(apply_manifest(pp, :catch_failures => true).exit_code).to be_zero
+    end
+
+    describe package('ruby1.9.1') do
+      it {
+        should be_installed
+      }
+    end
+  end
+
+  context 'with ruby 1.8.7 specified' do
+    it 'should run with no errors' do
+      pp = <<-EOS
+      class { 'ruby':
+        version         => '1.8.7',
+        rubygems_update => false
+      }
+      EOS
+
+      # Run it twice and test for idempotency
+      apply_manifest(pp, :catch_failures => true)
+      expect(apply_manifest(pp, :catch_failures => true).exit_code).to be_zero
+    end
+
+    describe package('rubygems') do
+      it {
+        should be_installed
+      }
+    end
+  end
+
+  context 'with latest rubygems' do
     it 'should run with no errors' do
       pp = <<-EOS
       class { 'ruby':
