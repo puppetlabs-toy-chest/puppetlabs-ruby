c441728212ca8e63c8f0a2890f8dcf132570d500
Wolf Noble <gitlab@wolfspyre.com>
2014-06-22 00:45:10 -0500

    This changes the default values for ruby_package and rubygems_package on ubuntu 1404.

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,18 +5,30 @@
 class ruby::params {
   $version              = 'installed'
   $gems_version         = 'installed'
-  $ruby_package         = 'ruby'
-  $rubygems_package     = 'rubygems'
   $ruby_switch_package  = 'ruby-switch'
 
   case $::osfamily {
     'redhat', 'amazon': {
-      $ruby_dev = ['ruby-devel','rubygems-bundler']
-      $rubygems_update = true
+      $ruby_dev             = ['ruby-devel','rubygems-bundler']
+      $rubygems_update      = true
+      $ruby_package         = 'ruby'
+      $rubygems_package     = 'rubygems'
     }
     'debian': {
-      $ruby_dev = [ 'ruby-dev', 'rake', 'ri', 'ruby-bundler', 'pkg-config' ]
-      $rubygems_update = false
+      $ruby_dev             = [ 'ruby-dev', 'rake', 'ri', 'ruby-bundler', 'pkg-config' ]
+      $rubygems_update      = false
+      case $::operatingsystemrelease {
+        '14.04': {
+          #Ubuntu 14.04 changed ruby/rubygems to be all in one package. Specifying these as defaults will permit the module to behave as anticipated.
+          $ruby_package     = 'ruby1.9.1'
+          $rubygems_package = 'ruby1.9.1-full'
+        }
+        default: {
+          $ruby_package     = 'ruby'
+          $rubygems_package = 'rubygems'
+        }
+
+      }
     }
     default: {
       fail("Unsupported OS family: ${::osfamily}")
