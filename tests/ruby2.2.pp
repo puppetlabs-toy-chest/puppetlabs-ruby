bdbf4533e7a04606b020457497abd2e2054fb279
Tom Simnett <tom@initforthe.com>
2016-10-05 16:24:30 +0100

    Space removal to please linter

--- tests/ruby2.2.pp
+++ tests/ruby2.2.pp
@@ -1,4 +1,4 @@
 class{'ruby':
-  version             => '2.2',
-  set_system_default  => true,
+  version            => '2.2',
+  set_system_default => true,
 }
--- tests/ruby2.3.pp
+++ tests/ruby2.3.pp
@@ -1,4 +1,4 @@
 class{'ruby':
-  version             => '2.3',
-  set_system_default  => true,
+  version            => '2.3',
+  set_system_default => true,
 }
